ca74bd296d5a40f151499bfdacb5dd720dcf48fd
Benjamin Houghton <bth51@georgetown.edu>
2022-02-16 11:24:00 -0500

    Commit the first version of sts_pearson.py and sts_metrics.py

---
+++ sts_metrics.py
@@ -0,0 +1,189 @@
+from nltk.translate.nist_score import sentence_nist
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+from difflib import SequenceMatcher
+from util import parse_sts
+from nltk import edit_distance
+
+
+
+def symmetrical_nist(texts):
+    """
+    Calculates symmetrical similarity as NIST(a,b) + NIST(b,a).
+
+    Arguments:
+        texts (tuple): The sentences to compare. Represented as a tuple
+        containing two lists of the tokenized sentences, where each list
+        represents one of the sentences i.e.
+        (["this", "is", "sentence", "one"], ["here", "is", "sentence" "2"]).
+
+    Returns:
+        nist_score (float): The symmetrical NIST score of the 2 sentences.
+    """
+
+    # Define the tokens to compare
+    t1_tokens, t2_tokens = texts
+
+    # Try to calculate the first score, zero division is possible due to
+    # the lowest score being 0
+    try:
+        nist_1 = sentence_nist([t1_tokens, ], t2_tokens)
+
+    # If zero division occurs raise error and set first score to 0
+    except ZeroDivisionError as err:
+        print('No NIST', err)
+        # Set the score to 0
+        nist_1 = 0.0
+
+    # Try to calculate the second score, zero division is possible due to
+    # the lowest score being 0
+    try:
+        nist_2 = sentence_nist([t2_tokens, ], t1_tokens)
+
+    # If zero division occurs raise error and set second score to 0
+    except ZeroDivisionError as err:
+        print('No NIST', err)
+        # Set the score to 0
+        nist_2 = 0.0
+
+    # Add scores to get final score value
+    nist_score = nist_1 + nist_2
+
+    return nist_score
+
+
+def symmetrical_bleu(texts):
+    """
+    Calculates symmetrical similarity as BLEU(a,b) + BLEU(b,a).
+
+    Arguments:
+        texts (tuple): The sentences to compare. Represented as a tuple
+        containing two lists of the tokenized sentences, where each list
+        represents one of the sentences i.e.
+        (["this", "is", "sentence", "one"], ["here", "is", "sentence" "2"]).
+
+    Returns:
+        bleu_score (float): The symmetrical BLEU score of the 2 sentences.
+    """
+
+    # Define the tokens to compare
+    t1_tokens, t2_tokens = texts
+
+    # # Try to calculate the first score, zero division is possible due to
+    # # the lowest score being 0
+    try:
+        bleu_1 = sentence_bleu(
+            [t1_tokens, ],
+            t2_tokens,
+            smoothing_function=SmoothingFunction().method0
+        )
+
+    # If zero division occurs raise error and set first score to 0
+    except ZeroDivisionError as err:
+        print('No BLEU', err)
+        # Set the score to 0
+        bleu_1 = 0.0
+
+    # Try to calculate the second score, zero division is possible due to
+    # the lowest score being 0
+    try:
+        bleu_2 = sentence_bleu(
+            [t2_tokens, ],
+            t1_tokens,
+            smoothing_function=SmoothingFunction().method0
+        )
+
+    # If zero division occurs raise error and set second score to 0
+    except ZeroDivisionError as err:
+        print('No BLEU', err)
+        # Set the score to 0
+        bleu_2 = 0.0
+
+    # Add scores to get final score value
+    bleu_score = bleu_1 + bleu_2
+
+    return bleu_score
+
+
+def symmetrical_word_error(texts):
+    """
+    Calculates the symmetrical word error rate of two tokenized texts.
+
+    Arguments:
+        texts (tuple): The sentences to compare. Represented as a tuple
+            containing two lists of the tokenized sentences, where each list
+            represents one of the sentences i.e.
+            (["this", "is", "sentence", "one"], ["here", "is", "sentence" "2"]).
+
+    Returns:
+        score (float): The symmetrical word error rate score.
+
+    """
+
+    # Define the tokens to compare
+    t1_tokens, t2_tokens = texts
+
+    edit_distance_score = edit_distance(t1_tokens, t2_tokens)
+
+    score = edit_distance_score/len(t1_tokens) + \
+        edit_distance_score/len(t2_tokens)
+
+    return score
+
+
+def longest_common(texts):
+    """
+    Determines the longest common substring of two texts.
+
+    Arguments:
+        texts (tuple): The sentences to compare. Represented as a tuple
+        containing with a single string for each sentence, i.e.
+        ("This is sentence one.", "Here is sentence 2.").
+
+    Returns:
+        score (int): The length of the longest common substring.
+    """
+
+    # Define the tokens to compare
+    text1, text2 = texts
+
+    # Instantiate the sequence matcher
+    matcher = SequenceMatcher(None, text1, text2)
+
+    # Get the results from the sequence matcher
+    results = matcher.find_longest_match(0, len(text1), 0, len(text2))
+
+    # Get the longest common substring value
+    score = results[2]
+
+    return score
+
+
+def edit_dist(texts):
+    """
+    Calculates the Levenshtein edit-distance between two strings.
+
+    Arguments:
+        texts (tuple): The sentences to compare. Represented as a tuple
+            containing with a single string for each sentence, i.e.
+            ("This is sentence one.", "Here is sentence 2.").
+
+    Returns:
+        score (int): The Levenshtein edit-distance score.
+    """
+
+    # Define the tokens to compare
+    text1, text2 = texts
+
+    # Calculate the edit distance score
+    score = edit_distance(text1, text2)
+
+    return score
+
+
+if __name__ == '__main__':
+
+    sts_data = 'stsbenchmark/sts-dev.csv'
+    txts, lbls = parse_sts(sts_data)
+    sim_metrics = SimilarityMetrics(txts, lbls)
+
+    print(sim_metrics.tokens)
--- sts_pearson.py
+++ sts_pearson.py
@@ -1,6 +1,63 @@
-from scipy.stats import pearsonr
 import argparse
+import numpy as np
 from util import parse_sts
+from nltk.tokenize import word_tokenize
+from sts_metrics import symmetrical_nist, symmetrical_bleu, longest_common, \
+    symmetrical_word_error, edit_dist
+from scipy.stats import pearsonr
+
+
+def generate_scores(
+        current_text: tuple,
+        token_metrics: dict,
+        string_metrics: dict,
+        scores: dict
+) -> None:
+    """
+    Calculates and stores the score of various string similarity metrics.
+
+    Arguments:
+        current_text (tuple): The sentences to compare. Represented as a
+            tuple containing with a single string for each sentence, i.e.
+            ("This is sentence one.", "Here is sentence 2.").
+        token_metrics (dict): The metrics which require the texts to be
+            tokenized, with keys as the metric names and values as the function
+            to calculate the score.
+        string_metrics (dict): The metrics which require the texts to be
+            represented as single/complete strings, with keys as the metric
+            names and values as the function to calculate the score.
+        scores (dict):
+            The score dictionary to update for each metric.
+
+    """
+
+    # Define the texts to be compared
+    text1, text2 = current_text[0].lower(), current_text[1].lower()
+
+    text1 = text1.lower()
+    text2 = text2.lower()
+
+    # Tokenize the texts
+    t1_tokens = word_tokenize(text1)
+    t2_tokens = word_tokenize(text2)
+
+    # Iterate over all metrics that require tokens
+    for metric, method in token_metrics.items():
+
+        # Get the metric score
+        metric_score = method((t1_tokens, t2_tokens))
+
+        # Add the score to the score dictionary for current metric and texts
+        scores[metric].append(metric_score)
+
+    # Iterate over all metrics that require single string sentences
+    for metric, method in string_metrics.items():
+
+        # Get the score in each order
+        metric_score = method((text1, text2))
+
+        # Add the score to the score dictionary for current metric and texts
+        scores[metric].append(metric_score)
 
 
 def main(sts_data):
@@ -8,7 +65,18 @@ def main(sts_data):
     Data is formatted as in the STS benchmark"""
 
     # TODO 1: read the dataset; implement in util.py
+
+    # Read in the data set texts and labels
     texts, labels = parse_sts(sts_data)
+
+    # Convert the texts to lower case and store in numpy array
+    texts = np.array([[text.lower() for text in pair] for pair in texts], dtype=object)
+
+    # Convert the texts to tokens and place into numpy array
+    tokens = np.array([[word_tokenize(text) for text in pair] for pair in texts], dtype=object)
+
+    # Convert the labels to a numpy array of floats
+    labels = np.array(labels).astype(float)
 
     print(f"Found {len(texts)} STS pairs")
 
@@ -16,15 +84,48 @@ def main(sts_data):
     # HINT: Longest common substring can be complicated. Investigate difflib.SequenceMatcher for a good option.
     score_types = ["NIST", "BLEU", "Word Error Rate", "Longest common substring", "Edit Distance"]
 
-    #TODO 3: Calculate pearson r between each metric and the STS labels and report in the README.
+    # Instantiate a dictionary to store the scores of each metric
+    scores = {metric: [] for metric in score_types}
+
+    # Define the token metrics and their function calls as key, value pairs
+    token_metrics = {
+        'NIST': symmetrical_nist,
+        'BLEU': symmetrical_bleu,
+        'Word Error Rate': symmetrical_word_error,
+    }
+
+    # Define the string metrics and their function calls as key, value pairs
+    string_metrics = {
+        'Longest common substring': longest_common,
+        'Edit Distance': edit_dist
+    }
+
+    # Iterate over all texts in the corpus
+    for text_pair in texts:
+
+        # Generate the score for each metric for the current text pair
+        generate_scores(text_pair, token_metrics, string_metrics, scores)
+
+    # TODO 3: Calculate pearson r between each metric and the STS labels and report in the README.
     # Sample code to print results. You can alter the printing as you see fit. It is most important to put the results
     # in a table in the README
     print(f"Semantic textual similarity for {sts_data}\n")
     for metric_name in score_types:
-        score = 0.0
-        print(f"{metric_name} correlation: {score:.03f}")
+
+        if metric_name in []:
+            pass
+
+        else:
+            # Calculate the pearson correlation coefficient with predicted
+            # similarity score and the true (human labeled) similarity score
+            score = pearsonr(scores[metric_name], labels)
+
+            # Print the score to stdout via print command
+            print(f"{metric_name} correlation: {score[0]:.03f}")
+
 
     # TODO 4: Complete writeup as specified by TODOs in README (describe metrics; show usage)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -33,4 +134,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(args.sts_data)
+
 
