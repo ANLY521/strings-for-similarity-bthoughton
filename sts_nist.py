af08da8153043c0aa51096077999925078286c5e
Benjamin Houghton <bth51@georgetown.edu>
2022-02-16 11:58:40 -0500

    Commit the week 1 lab file, sts_nist.py

--- sts_nist.py
+++ sts_nist.py
@@ -5,16 +5,52 @@ import argparse
 import numpy as np
 
 
-def symmetrical_nist(text_pair):
+def symmetrical_nist(texts):
     """
     Calculates symmetrical similarity as NIST(a,b) + NIST(b,a).
-    :param text_pair: iterable to two strings to compare
-    :return: a float
-    """
-    nist_1 = 0.0
-    nist_2 = 0.0
 
-    return nist_1 + nist_2
+    Arguments:
+        texts (tuple): The sentences to compare. Represented as a tuple
+        containing with a single string for each sentence, i.e.
+        ("This is sentence one.", "Here is sentence 2.").
+
+    Returns:
+        nist_score (float): The symmetrical NIST score of the 2 sentences.
+    """
+
+    text1, text2 = texts
+
+    # input tokenized text
+    t1_tokens = word_tokenize(text1.lower())
+    t2_tokens = word_tokenize(text2.lower())
+
+    # Try to calculate the first score, zero division is possible due to
+    # the lowest score being 0
+    try:
+        nist_1 = sentence_nist([t1_tokens, ], t2_tokens)
+
+    # If zero division occurs raise error and set first score to 0
+    except ZeroDivisionError as err:
+        print('No NIST', err)
+        # Set the score to 0
+        nist_1 = 0.0
+
+    # Try to calculate the second score, zero division is possible due to
+    # the lowest score being 0
+    try:
+        nist_2 = sentence_nist([t2_tokens, ], t1_tokens)
+
+    # If zero division occurs raise error and set second score to 0
+    except ZeroDivisionError as err:
+        print('No NIST', err)
+        # Set the score to 0
+        nist_2 = 0.0
+
+    # Add scores to get final score value
+    nist_score = nist_1 + nist_2
+
+    return nist_score
+
 
 def main(sts_data):
     """Calculate NIST metric for pairs of strings
@@ -35,7 +71,7 @@ def main(sts_data):
     scores = []
     for label,text_pair in sample_data:
         print(label)
-        print(f"Sentences: {texts[0]}\t{texts[1]}")
+        print(f"Sentences: {text_pair[0]}\t{text_pair[1]}")
         # TODO 2: Calculate NIST for each pair of sentences
         # Define the function symmetrical_nist
 
@@ -46,13 +82,23 @@ def main(sts_data):
     # This assertion verifies that symmetrical_nist is symmetrical
     # if the assertion holds, execution continues. If it does not, the program crashes
     first_pair = texts[0]
-    print(first_pair)
     text_a, text_b = first_pair
     nist_ab = symmetrical_nist((text_a, text_b))
     nist_ba = symmetrical_nist((text_b, text_a))
     assert nist_ab == nist_ba, f"Symmetrical NIST is not symmetrical! Got {nist_ab} and {nist_ba}"
 
     # TODO 3: find and print the sentences from the sample with the highest and lowest scores
+    min_score_index = np.argmin(scores)
+    min_score = scores[min_score_index]
+    print(f"Lowest score: {min_score}")
+    print(sample_text[min_score_index])
+    assert min_score == symmetrical_nist(sample_text[min_score_index])
+
+    max_score_index = np.argmax(scores)
+    max_score = scores[max_score_index]
+    print(f"Highest score: {max_score}")
+    print(sample_text[max_score_index])
+    assert max_score == symmetrical_nist(sample_text[max_score_index])
 
 
 
